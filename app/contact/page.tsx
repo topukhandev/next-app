50e2678023f3b92bba94a234529f178ea05c6932
Tofayel Ahmmad Topu <topu.steadfast@gmail.com>
2024-07-02 18:39:47 +0600

    49 minute; upto error page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 export default function contactPage() {
+    throw new Error('page found')
     return (
         <main className="mt-5">
             <div>contactPage</div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>this is home page</main>
+    <main>
+        this is home page</main>
   );
 }
