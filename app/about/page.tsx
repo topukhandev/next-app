768cc9c3fa28f978f22dab7ed3acf4e6b5d23962
Tofayel Ahmmad Topu <topu.steadfast@gmail.com>
2024-07-02 18:15:29 +0600

    about and contact page add

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function aboutPage() {
+  return (
+    <main className='mt-5'>
+        <div>aboutPage</div>
+    </main>
+  )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function contactPage() {
+    return (
+        <main className="mt-5">
+            <div>contactPage</div>
+        </main>
+    );
+}
